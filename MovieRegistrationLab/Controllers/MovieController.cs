f0ac2a690d0a9c9998a07761d6f588aeeba68dfa
alex <acrang@gmail.com>
2020-11-16 08:59:45 -0500

    First commit for Movie Registration Lab

---
+++ MovieRegistrationLab/Controllers/MovieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistrationLab.Models;
+
+namespace MovieRegistrationLab.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieForm()
+        {
+            return View(new Movie());
+        }
+
+        public IActionResult DisplayMovie(Movie Film)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(Film);
+            }
+            else
+            {
+                return View("MovieForm", Film);
+            }
+        }
+    }
+}
